0104cb61c398a4a43431312430a11654d58e81bf
sohaibsalman <sohaibsalman10@gmail.com>
2023-07-25 10:27:39 -0700

    Render template from view function

--- playground/views.py
+++ playground/views.py
@@ -1,6 +1,9 @@
+from datetime import datetime
 from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def say_hello(request):
-    return HttpResponse("Hello World")
+def index(request):
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    return render(request, "index.html", { 'current_time': current_time })
