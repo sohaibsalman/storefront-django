46712a10a28dc260ed4f8a9582cc1fb1d1573db8
sohaibsalman <sohaibsalman10@gmail.com>
2023-07-29 14:34:53 +0500

    Create Generic relationships

--- tag/models.py
+++ tag/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
-# Create your models here.
+class Tag(models.Model):
+    label = models.CharField(max_length=255)
+
+
+class TagItem(models.Model):
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+
+    # For generic relation, we need 2 things
+    # (1) - Type of the object
+    # (2) - id of the object
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey()
