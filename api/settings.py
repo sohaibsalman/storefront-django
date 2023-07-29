45b296169f22b8e610c91dec57cdd53dcb36b90e
sohaibsalman <sohaibsalman10@gmail.com>
2023-07-29 15:27:14 +0500

    Configure MySQL database with django

--- api/settings.py
+++ api/settings.py
@@ -65,8 +65,11 @@ WSGI_APPLICATION = 'api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'storefront',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': '1234',
     }
 }
 
