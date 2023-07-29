aa0797be03b61a90c0dadbd2d657cd2fa07c92e5
sohaibsalman <sohaibsalman10@gmail.com>
2023-07-29 14:18:16 +0500

    Create one-to-many relationships between models

--- store/models.py
+++ store/models.py
@@ -1,11 +1,16 @@
 from django.db import models
 
+class Collection(models.Model):
+    title = models.CharField(max_length=255)
+
+
 class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
+    collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
 
 
 class Customer(models.Model):
@@ -40,6 +45,14 @@ class Order(models.Model):
 
     placed_at = models.DateTimeField(auto_now_add=True)
     payment_status = models.CharField(max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.PROTECT)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    quantity = models.PositiveSmallIntegerField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
 
 
 class Address(models.Model):
@@ -49,3 +62,13 @@ class Address(models.Model):
     # This will define 1-to-1 relationship with customer model
     # Django will automatically create a reverse property in Customer class
     customer = models.OneToOneField(Customer, on_delete=models.CASCADE, primary_key=True)
+
+
+class Cart(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField()
