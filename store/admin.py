1f4eba01701aed76a51dee59e9f608e5fd523109
sohaibsalman <sohaibsalman10@gmail.com>
2023-07-31 22:25:54 +0500

    Show related objects in admin site

--- store/admin.py
+++ store/admin.py
@@ -7,9 +7,19 @@ from . import models
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'unit_price']
+    list_display = ['title', 'unit_price', 'inventory_status', 'collection_title']
     list_editable = ['unit_price']
     list_per_page = 10
+    list_select_related = ['collection']
+
+    @admin.display(ordering='inventory')
+    def inventory_status(self, product: models.Product) -> str:
+        if product.inventory < 10:
+            return 'Low'
+        return 'Ok'
+    
+    def collection_title(self, product: models.Product) -> str:
+        return product.collection.title
 
 
 @admin.register(models.Customer)
@@ -19,5 +29,12 @@ class CustomerAdmin(admin.ModelAdmin):
     list_editable = ['membership']
     list_per_page = 10
 
+
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['placed_at', 'payment_status', 'customer']
+    ordering = ['placed_at', 'payment_status']
+    list_per_page = 10
+    list_select_related = ['customer']
 
 admin.site.register(models.Collection)
