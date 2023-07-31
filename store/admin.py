e312472785d86ae9c59c80e4a92f4eaf8ed6b526
sohaibsalman <sohaibsalman10@gmail.com>
2023-07-31 22:05:51 +0500

    Customizing lists by adding columns

--- store/admin.py
+++ store/admin.py
@@ -5,5 +5,19 @@ from . import models
 # Register your models here.
 # Every django app has admin.py file to customize the models registering
 
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'unit_price']
+    list_editable = ['unit_price']
+    list_per_page = 10
+
+
+@admin.register(models.Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'membership']
+    ordering = ['first_name', 'last_name']
+    list_editable = ['membership']
+    list_per_page = 10
+
+
 admin.site.register(models.Collection)
-admin.site.register(models.Product)
