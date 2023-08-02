2a2ba3487d8ad8fb7243037436b7379818e054d7
sohaibsalman <sohaibsalman10@gmail.com>
2023-08-02 14:31:25 +0500

    Create APIs for products list and details

--- store/views.py
+++ store/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+@api_view()
+def products_list(request):
+    return Response('Ok')
+
+
+@api_view()
+def product_details(request, id):
+    return Response(id)
