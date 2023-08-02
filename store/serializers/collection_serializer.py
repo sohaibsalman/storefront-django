866c9e5b1578224894382c3020b560183933986b
sohaibsalman <sohaibsalman10@gmail.com>
2023-08-02 20:01:54 +0500

    Use ModelSerialization to avoid code duplication

--- store/serializers/collection_serializer.py
+++ store/serializers/collection_serializer.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
+from ..models import Collection
 
-class CollectionSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Collection
+        fields = ['id', 'title']
--- store/serializers/product_serializer.py
+++ store/serializers/product_serializer.py
@@ -3,11 +3,14 @@ from rest_framework import serializers
 from ..models import Product, Collection
 from .collection_serializer import CollectionSerializer
 
-class ProductSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
-    price = serializers.DecimalField(max_digits=6, decimal_places=2, source='unit_price')
-    # Custom serializer field
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'unit_price', 'price_with_tax', 'collection']
+    # id = serializers.IntegerField()
+    # title = serializers.CharField(max_length=255)
+    # price = serializers.DecimalField(max_digits=6, decimal_places=2, source='unit_price')
+    
     price_with_tax = serializers.SerializerMethodField('calculate_tax')
     # collection = serializers.PrimaryKeyRelatedField(queryset=Collection.objects.all()) # To send collection foreign key
     # collection = serializers.StringRelatedField() # To send collection as string representation
